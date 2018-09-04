b728074427f8fa486e9016d74b5d6bb976c48477
hzy <hzyall@163.com>
2018-09-04 21:54:03 +0800

    Spring-Boot-Starter-aop

---
+++ src/main/java/com/hzy/girl/HttpAspect.java
@@ -0,0 +1,55 @@
+package com.hzy.girl;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
+@Component
+public class HttpAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(HttpAspect.class);
+
+    @Pointcut("execution(public * com.hzy.girl.controller.*.*(..))")
+    public void pointCut(){
+    }
+
+    @Before("pointCut()")
+    public void before(JoinPoint joinPoint) {
+
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = (HttpServletRequest) attributes.getRequest();
+
+        //url
+        logger.info("url={}", request.getRequestURL());
+
+        //method
+        logger.info("method={}", request.getMethod());
+
+        //ip
+        logger.info("ip={}", request.getRemoteAddr());
+
+        //class.methodName
+        logger.info("class.methodName={}", joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+
+        //参数
+        logger.info("args={}", joinPoint.getArgs());
+
+    }
+
+    @AfterReturning(returning = "object", pointcut = "pointCut()")
+    public void doAfterReturning(Object object) {
+        logger.info("response = {}", object);
+    }
+
+
+}
--- src/main/java/com/hzy/girl/controller/GirlController.java
+++ src/main/java/com/hzy/girl/controller/GirlController.java
@@ -3,6 +3,7 @@ package com.hzy.girl.controller;
 import com.hzy.girl.entity.Girl;
 import com.hzy.girl.reponsitory.GirlReponsitory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,7 @@ public class GirlController {
      * @return
      */
     @GetMapping(value = "girls")
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public List<Girl> girlList() {
         return girlReponsitory.findAll();
     }
